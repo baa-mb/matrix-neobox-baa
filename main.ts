780b446f3a89fb1905f13ee806252123e9961f50
baa <34665120+baa-mb@users.noreply.github.com>
2021-09-26 20:29:21 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showIcon(IconNames.Diamond)
+basic.showString("DLPL")
+radio.setGroup(54)
+DLPLmatrix.definitionMatrix(nrMatrix.zentral, HWMatrix.m0, 1)
+DLPLmatrix.schreibeText(
+nrMatrix.zentral,
+"DLPL",
+0xff0000,
+false
+)
